0eb1c8512e60452187b86ed54d99e669af1acd64
wezleyp <wesley.patterson121@gmail.com>
2022-02-27 17:15:44 -0600

    this is it

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        yururrrrrrrrrrrrrr
+      </p>
+      <div>
+        akljsdkjfkljkl
+      </div>
+    </div>
+  );
+}
+
+export default App;
